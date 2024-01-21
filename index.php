1ed509aff4f6c587abbcee955f93702a443f5410
Diogoterapia <156975154+Diogoterapia@users.noreply.github.com>
2024-01-21 20:04:11 -0300

    Melhorias
    Melhorias

---
+++ index.php
@@ -0,0 +1,34 @@
+<!Doctype html>
+
+
+<head>
+
+<br><br><br><br><br>
+
+<title>Chocolates</title>
+
+<h1 style='text-align: center'>CHOCOLATES</h1>
+
+<container>
+
+<div class='row' style='text-align: center'>
+
+<h1>teste</h1>
+<h2>teste</h2>
+
+
+
+</div>
+</container>
+
+</head>
+<body>
+
+
+
+
+
+
+</body>
+
+</html5>
